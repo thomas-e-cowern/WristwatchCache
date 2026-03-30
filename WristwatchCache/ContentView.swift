65c36ca5d35c015269ad33b3872aeb1025428737
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-30 11:13:55 -0400

    update ContentView, fix preview to show sampleData

--- WristwatchCache/ContentView.swift
+++ WristwatchCache/ContentView.swift
@@ -6,19 +6,62 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var watches: [Watch]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(watches) { watch in
+                    HStack {
+                        Text(watch.brand)
+                            .font(.title)
+                            .foregroundColor(.white) // Color of the text
+                            .padding(20) // Adds space between text and circle
+                            .background(Color.blue) // Color of the circle
+                            .clipShape(Circle()) // Clips the background color to a circle shape
+                        Text(watch.model)
+                            .font(.headline)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    let preview = Preview(Watch.self)
+    preview.addSamples(Watch.sampleData)
+    return ContentView()
+        .modelContainer(preview.container)
+}
+
+import Foundation
+import SwiftData
+
+struct Preview {
+    let container: ModelContainer
+    
+    init(_ models: any PersistentModel.Type...) {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let schema = Schema(models)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("The preview container couldn't be created")
+        }
+    }
+    
+    func addSamples(_ watches: [Watch]) {
+        Task { @MainActor in
+            watches.forEach { watch in
+                container.mainContext.insert(watch)
+            }
+        }
+    }
 }
