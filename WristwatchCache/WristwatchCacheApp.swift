8d0c88ced9443b2e50caa84755046e3c72ea638b
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-30 10:04:11 -0400

    WristwatchCache initial commi

---
+++ WristwatchCache/WristwatchCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristwatchCacheApp.swift
+//  WristwatchCache
+//
+//  Created by Thomas Cowern on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct WristwatchCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
