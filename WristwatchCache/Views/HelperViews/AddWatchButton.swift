2807174feedaa71dcc38934f0f9017afec870170
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-03-31 11:37:17 -0400

    add AddWatchButton to HelperViews

---
+++ WristwatchCache/Views/HelperViews/AddWatchButton.swift
@@ -0,0 +1,23 @@
+//
+//  AddWatchButton.swift
+//  WristwatchCache
+//
+//  Created by Thomas Cowern on 3/31/26.
+//
+
+import SwiftUI
+
+struct AddWatchButton: View {
+    @State private var showAddWatch = false
+
+    var body: some View {
+        Button {
+            showAddWatch = true
+        } label: {
+            Image(systemName: "plus")
+        }
+        .sheet(isPresented: $showAddWatch) {
+            AddWatchView()
+        }
+    }
+}
