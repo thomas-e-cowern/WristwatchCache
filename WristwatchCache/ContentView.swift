87076e7ccd92c39200433339dc4ddb3a36bcf111
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-04-01 14:14:17 -0400

    update ContentView to fix tabs

--- WristwatchCache/ContentView.swift
+++ WristwatchCache/ContentView.swift
@@ -16,13 +16,16 @@ struct ContentView: View {
     
     var body: some View {
         TabView(selection: $selection) {
-            Tab("Watch List", systemImage: "list.dash", value: 0) {
+            Tab("On Wrist", systemImage: "watch.analog", value: 0) {
+                OnWristView()
+            }
+            Tab("Watch List", systemImage: "list.dash", value: 1) {
                 WatchListView()
             }
-            Tab("Favorites List", systemImage: "heart", value: 1) {
+            Tab("Favorites List", systemImage: "heart", value: 2) {
                 WatchFavoritesView()
             }
-            Tab("Wrist Stats", systemImage: "chart.bar", value: 2) {
+            Tab("Wrist Stats", systemImage: "chart.bar", value: 3) {
                 WristStatsView()
             }
         }
