14f7b6b612b85f36d27ca462d15918f9a7b48a0e
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-04-07 11:48:44 -0400

    update WatchSearchObservable to effectively handle search cases

--- WristwatchCache/Observable/WatchSearchObservable.swift
+++ WristwatchCache/Observable/WatchSearchObservable.swift
@@ -4,3 +4,21 @@
 //
 //  Created by Thomas Cowern on 4/7/26.
 //
+
+import Foundation
+import Observation
+
+@Observable
+class WatchSearchObservable {
+    var searchText: String = ""
+
+    func filteredResults(from watches: [Watch]) -> [Watch] {
+        if searchText.isEmpty {
+            return watches
+        } else {
+            return watches.filter {
+                $0.brand.localizedStandardContains(searchText) || $0.model.localizedStandardContains(searchText)
+            }
+        }
+    }
+}
