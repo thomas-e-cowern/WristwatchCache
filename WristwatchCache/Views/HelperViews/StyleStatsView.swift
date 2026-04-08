36a652e6c65556b7a81bacf8b9b2857361629ff9
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-04-08 09:26:02 -0400

    add StyleStatsView to HelperViews folder

---
+++ WristwatchCache/Views/HelperViews/StyleStatsView.swift
@@ -0,0 +1,58 @@
+//
+//  StyleStatsView.swift
+//  WristwatchCache
+//
+//  Created by Thomas Cowern on 4/8/26.
+//
+
+import SwiftUI
+
+struct StyleStatsView: View {
+    let label: String
+    let color: Color
+    let number: Int
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(color)
+            VStack(spacing: 4) {
+                Text(label)
+                    .font(.title2)
+                    .foregroundColor(idealTextColor(for: color))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.6)
+                Text("\(number)")
+                    .font(.title)
+                    .bold()
+                    .foregroundColor(idealTextColor(for: color))
+            }
+            .padding(12)
+        }
+        .frame(width: 196, height: 196)
+        .accessibilityElement(children: .combine)
+        .accessibilityLabel("\(label), \(number)")
+    }
+
+    // Choose black or white text depending on background luminance for contrast
+    private func idealTextColor(for background: Color) -> Color {
+        #if canImport(UIKit)
+        // Convert SwiftUI Color to UIColor
+        let uiColor = UIColor(background)
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        #else
+        let nsColor = NSColor(background)
+        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
+        nsColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        #endif
+
+        // Perceived luminance
+        let luminance = 0.299 * Double(red) + 0.587 * Double(green) + 0.114 * Double(blue)
+        return luminance > 0.6 ? .black : .white
+    }
+}
+
+#Preview {
+    StyleStatsView(label: "Rolex", color: Color.green, number: 2)
+}
