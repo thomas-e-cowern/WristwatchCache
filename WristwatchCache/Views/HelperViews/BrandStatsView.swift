dfaa2c4dbfeb54d1e7963b3604b0a4f87ecb3c18
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2026-04-24 07:55:27 -0400

    update BrandStatsView, MovementStatsView, StyleStatsView to use Color+IdealTextColor

--- WristwatchCache/Views/HelperViews/BrandStatsView.swift
+++ WristwatchCache/Views/HelperViews/BrandStatsView.swift
@@ -19,37 +19,19 @@ struct BrandStatsView: View {
             VStack(spacing: 4) {
                 Text(label)
                     .font(.title2)
-                    .foregroundColor(idealTextColor(for: color))
+                    .foregroundColor(color.idealTextColor)
                     .lineLimit(1)
                     .minimumScaleFactor(0.6)
                 Text("\(number)")
                     .font(.title)
                     .bold()
-                    .foregroundColor(idealTextColor(for: color))
+                    .foregroundColor(color.idealTextColor)
             }
             .padding(12)
         }
         .frame(width: 196, height: 196)
         .accessibilityElement(children: .combine)
         .accessibilityLabel("\(label), \(number)")
-    }
-
-    // Choose black or white text depending on background luminance for contrast
-    private func idealTextColor(for background: Color) -> Color {
-        #if canImport(UIKit)
-        // Convert SwiftUI Color to UIColor
-        let uiColor = UIColor(background)
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        #else
-        let nsColor = NSColor(background)
-        var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0, alpha: CGFloat = 0
-        nsColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
-        #endif
-
-        // Perceived luminance
-        let luminance = 0.299 * Double(red) + 0.587 * Double(green) + 0.114 * Double(blue)
-        return luminance > 0.6 ? .black : .white
     }
 }
 
